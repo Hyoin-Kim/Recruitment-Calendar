1eaf387d881e961311e239c7a7652012546bff60
Hyoin-kim <hi980506@naver.com>
2021-12-17 00:25:06 +0900

    chore : 폴더 구조 세팅하기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
-function App() {
-  return <div className="App">안녕하세요</div>;
-}
+import React from "react";
+import Main from "./pages/Main";
+import Header from "./components/common/Header";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Main />
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/main/Dates.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Dates = () => {
+  return <div></div>;
+};
+
+export default Dates;
