caef5e6a5a161b36a1ddc80855c35bb4449768c5
Hyoin-kim <hi980506@naver.com>
2021-12-17 00:24:09 +0900

    chore : 필요한 에셋 export 해오기

---
+++ src/assets/index.js
@@ -0,0 +1,5 @@
+export { default as logo } from "./images/logoImage.png";
+export { default as LeftOff } from "./images/LeftOff.svg";
+export { default as LeftOn } from "./images/LeftOn.svg";
+export { default as RightOff } from "./images/RightOff.svg";
+export { default as RightOn } from "./images/RightOn.svg";
