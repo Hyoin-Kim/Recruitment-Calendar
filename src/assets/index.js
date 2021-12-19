be8227b4e5516af14215d6a00ef6e0e7a37557c8
Hyoin-Kim <hi980506@naver.com>
2021-12-19 21:52:48 +0900

    chore : 필요한 에셋 이미지 export 해오기

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,3 +3,5 @@ export { default as LeftOff } from "./images/LeftOff.svg";
 export { default as LeftOn } from "./images/LeftOn.svg";
 export { default as RightOff } from "./images/RightOff.svg";
 export { default as RightOn } from "./images/RightOn.svg";
+export { default as closeIcon } from "./images/close.png";
+export { default as chatIcon } from "./images/chat.png";
